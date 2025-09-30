07ffc7d8b50a52c4514c9f8c84add9934dd4b8d0
Duartois <matheusdugoncalves@gmail.com>
2025-09-30 12:33:29 -0300

    some ajusts

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -3,17 +3,17 @@ import localFont from "next/font/local";
 export const neueMontreal = localFont({
   src: [
     {
-      path: "../app/fonts/PPNeueMontreal-Regular.woff2",
+      path: "../app/fonts/NeueMontreal-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../app/fonts/PPNeueMontreal-Medium.woff2",
+      path: "../app/fonts/NeueMontreal-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../app/fonts/PPNeueMontreal-Bold.woff2",
+      path: "../app/fonts/NeueMontreal-Bold.woff2",
       weight: "700",
       style: "normal",
     },
