a97bafbc6d18a350912025bf8dbd9d5d63bc4d8c
Duartois <matheusdugoncalves@gmail.com>
2025-10-02 23:01:51 -0300

    some ajusts

--- components/Noise.tsx
+++ components/Noise.tsx
@@ -1,12 +1,10 @@
-import classNames from "classnames";
-
 import noise from "@/public/noise.png";
 
 export default function Noise({ className = "" }) {
   return (
     <div
       aria-hidden
-      className={classNames("pointer-events-none fixed inset-0 z-30", className)}
+      className="pointer-events-none fixed inset-0 z-30"
       style={{
         backgroundImage: `url(${noise.src})`,
         backgroundRepeat: "repeat",
--- components/three/CanvasRoot.tsx
+++ components/three/CanvasRoot.tsx
@@ -36,7 +36,7 @@ export default function CanvasRoot({ isReady }: CanvasRootProps) {
   }
 
   return (
-    <div className="fixed top-0 left-0 h-full w-full -z-10">
+    <div className="fixed top-0 left-0 h-full w-full z-0">
       <div
         className={classNames(
           "pointer-events-none fixed inset-0 -z-10 overflow-visible transition-opacity duration-700",
@@ -45,7 +45,7 @@ export default function CanvasRoot({ isReady }: CanvasRootProps) {
         aria-hidden={!isVisible}
       >
         <div className="relative h-full w-full">
-          <div className="absolute inset-0 -z-10">
+          <div className="absolute inset-0 z-10">
             <CoreCanvas />
           </div>
         </div>
