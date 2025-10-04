a98320689f6969b44e64f52ec7de5ee8a4d8cf9c
Duartois <matheusdugoncalves@gmail.com>
2025-10-03 21:50:47 -0300

    some ajusts

--- components/three/CoreCanvas.tsx
+++ components/three/CoreCanvas.tsx
@@ -50,7 +50,6 @@ export default function CoreCanvas({ isReady }: CoreCanvasProps) {
       return;
     }
 
-    handleRef.current.setState({ primordialRevealComplete: isReady });
   }, [isReady]);
 
   return (
